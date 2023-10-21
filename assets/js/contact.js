a92b0e03043f868d4d3a990472430937dbb77907
Daniel León Rua <116767540+LeonArt02@users.noreply.github.com>
2023-10-21 00:18:49 -0300

    Add files via upload

---
+++ assets/js/contact.js
@@ -0,0 +1,38 @@
+
+function contact_post () {
+    console.log("esta entrando a contact")
+    let formulario = document.getElementById('contact_form');
+    let name = document.getElementById('nombre').value;
+    let email = document.getElementById('correo').value;
+    let message = document.getElementById('mensaje').value;
+
+    console.log(name, email, message);
+    fetch('https://sheetdb.io/api/v1/yx651f3r93qnt', {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        data: [
+            {
+                "name": name,
+                "email": email,
+                "message": message
+            }
+        ]
+    })
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+  //   formulario.reset();
+}
+
+ function desplegar() {
+      var x = document.getElementById("CodingMusicTopNav");
+      if (x.className === "topnav") {
+      x.className += " responsive";
+      } else {
+      x.className = "topnav";
+      }
+    }
